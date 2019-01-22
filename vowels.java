e98ca6f6ba9d422ba332fdf385e91d17c6b77dc6
Mohankumar Shan <38549780+MohanKumar2123@users.noreply.github.com>
2019-01-22 16:48:41 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class vowels
+ {
+public static void main(String[] args)throws IOException
+{
+	boolean Vowel=false;;
+Scanner Sc = new Scanner(System.in);
+char ch = Sc.next().charAt(0);
+
+ switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+			case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U': Vowel = true;
+             
+                
+        }
+		
+		if(Vowel == true){
+		System.out.println("vowel");	
+		}
+		else {
+			
+			if((ch >= 'a' && ch <= 'z')|| ( ch >='A' && ch <= 'Z' )){
+				System.out.println("consonant");
+			}
+			else {
+				System.out.println("invalid");
+			}
+			
+		}
+
+	}	
+}
+
+
+
+
+
