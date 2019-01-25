df040ee0fff87981f4db48b3877f1c0e5f4dadeb
Mohankumar Shan <38549780+MohanKumar2123@users.noreply.github.com>
2019-01-25 10:43:46 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Hello{
+public static void main(String[] args){
+Scanner Sc = new Scanner(System.in);
+int N = Sc.nextInt();
+
+for(int i=0; i<N; i++){
+System.out.println("Hello");
+}
+}
+}
